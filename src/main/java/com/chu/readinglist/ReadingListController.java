023f2584d5a4df0648275f4170f50d441a10b3a1
zhurongzeng <ex-zhurongzeng@dashuf.com>
2018-01-09 09:47:06 +0800

    修改

--- src/main/java/com/chu/readinglist/ReadingListController.java
+++ src/main/java/com/chu/readinglist/ReadingListController.java
@@ -3,13 +3,13 @@ package com.chu.readinglist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/readinglist")
 public class ReadingListController {
 	@Autowired
